419dcceb2c4e8fa36faa7076d29e8ecf74da2f3f
Ozawa Masayuki <ozawa@coroq.com>
2018-08-31 09:29:30 +0900

    Add test and vendor directory to the target of phan

--- .phan/config.php
+++ .phan/config.php
@@ -3,6 +3,11 @@
 return [
   "directory_list" => [
     "src",
+    "test",
+    "vendor",
+  ],
+  "exclude_analysis_directory_list" => [
+    "vendor",
   ],
   "backward_compatibility_checks" => true,
 ];
